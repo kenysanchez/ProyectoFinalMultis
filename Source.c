35d18abb330bfb6c318ad69fc8f738c1c4317e55
Kenia Sánchez <kenya.gaby@hotmail.com>
2023-05-28 16:14:30 -0600

    feat: project base

--- Source.c
+++ Source.c
@@ -2,6 +2,19 @@
 
 int main() {
 
+	//Inicializacion
+	int matAColumna, matAFila, matBColumna, matBFila;
 
+	printf("Matriz A\n");
+	printf("Ingresa la cantidad de columnas de la matriz: \n");
+	scanf_s("%d", &matAColumna);
+	printf("Ingresa la cantidad de filas de la matriz: \n");
+	scanf_s("%d", &matAFila);
+
+	printf("\nMatriz B\n");
+	printf("Ingresa la cantidad de columnas de la matriz: \n");
+	scanf_s("%d", &matBColumna);
+	printf("Ingresa la cantidad de filas de la matriz: \n");
+	scanf_s("%d", &matBFila);
 	return 0;
 }
