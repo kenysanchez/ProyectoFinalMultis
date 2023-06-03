955933786d402ea5693beeff305d63451403f3df
DRogerSC <musicfan211@hotmail.com>
2023-06-03 15:13:35 -0600

    Added transpose function

--- IO/Matrix.c
+++ IO/Matrix.c
@@ -2,4 +2,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void transposeArray (double *entryMat, double *exitMat, int cols, int rows){
+
+    double *pointerAux, *pointerExit;
+    bool exit = false;
+    pointerAux = entryMat;
+    pointerExit = exitMat;
+    int noIteration;
+{
+        
+        for(int i = 0; i < cols; i++)
+            {
+                for(int j = 0; j < rows; j++)
+                {
+                    pointerAux = entryMat + (i+(j*cols));
+                    *pointerExit = *pointerAux;
+                    pointerExit += 1;
+                }
+            }
+    
+}
+
 
