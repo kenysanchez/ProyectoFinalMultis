b906651ea8a8940b7508876dc9d2cf330751ade4
DRogerSC <musicfan211@hotmail.com>
2023-06-06 22:48:48 -0600

    Updated deploy for use in server

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-gcc -Wall -c IO/Matrix.c
-gcc -Wall -c IO/Results.c
-gcc -Wall -c IO/Validation.c
-gcc -fopenmp -Wall -mavx512f -mavx512vl -c Methods/Parallel1.c
-gcc -Wall -c Methods/Parallel2.c
-gcc -Wall -c Methods/Serial.c
-gcc -Wall -c Program.c
-gcc -fopenmp -Wall -mavx512f -mavx512vl -O -o program Matrix.o Parallel1.o Parallel2.o Serial.o Results.o Validation.o Program.o
+gcc -Wall -c -std=c99 IO/Matrix.c
+gcc -Wall -c -std=c99 IO/Results.c
+gcc -Wall -c -std=c99 IO/Validation.c
+gcc -fopenmp -std=c99 -Wall -mavx512f -mavx512vl -c Methods/Parallel1.c
+gcc -Wall -c -std=c99 Methods/Parallel2.c
+gcc -Wall -c -std=c99 Methods/Serial.c
+gcc -Wall -c -std=c99 Program.c
+gcc -fopenmp -std=c99 -Wall -mavx512f -mavx512vl -O -o program Matrix.o Parallel1.o Parallel2.o Serial.o Results.o Validation.o Program.o
 
 
 ./program
